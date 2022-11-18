adc541011672e6564b188b4dfe6dc87a491e3a77
alejandro-piguave <alejandro.piguave.illescas@gmail.com>
2022-11-18 23:12:39 +0100

    Implement gallery access and camera logic.

--- app/src/main/java/com/apiguave/tinderclonecompose/AddPictureView.kt
+++ app/src/main/java/com/apiguave/tinderclonecompose/AddPictureView.kt
@@ -1,5 +1,8 @@
 package com.apiguave.tinderclonecompose
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -10,17 +13,19 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.apiguave.tinderclonecompose.fileprovider.ComposeFileProvider
 import com.apiguave.tinderclonecompose.ui.theme.LightPurple
 import com.apiguave.tinderclonecompose.ui.theme.Orange
 import com.apiguave.tinderclonecompose.ui.theme.Pink
@@ -28,10 +33,29 @@ import com.apiguave.tinderclonecompose.ui.theme.Purple
 
 @Composable
 fun AddPictureView(onCloseClicked: () -> Unit){
+    val galleryLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            // process eith the received image uri
+        }
+
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture(),
+        onResult = { success ->
+
+        }
+    )
+
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+
     Column(
         Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp, vertical = 16.dp)) {
+
+
         Icon(
             Icons.Filled.Close,
             modifier = Modifier
@@ -51,7 +75,8 @@ fun AddPictureView(onCloseClicked: () -> Unit){
             text2 = "Foto",
             color1 = Pink,
             color2 = Orange,
-            iconRes = R.drawable.ic_baseline_image_90
+            iconRes = R.drawable.ic_baseline_image_90,
+            onClick = {galleryLauncher.launch("image/*")}
         )
 
         Spacer(Modifier.height(16.dp))
@@ -60,16 +85,27 @@ fun AddPictureView(onCloseClicked: () -> Unit){
             text2 = "Cámara",
             color1 = Purple,
             color2 = LightPurple,
-            iconRes = R.drawable.ic_baseline_photo_camera_90
+            iconRes = R.drawable.ic_baseline_photo_camera_90,
+            onClick = {
+                val uri = ComposeFileProvider.getImageUri(context)
+                imageUri = uri
+                cameraLauncher.launch(uri)}
         )
         Spacer(Modifier.weight(1f))
     }
 }
 
 @Composable
-fun SourceTypeCard(text1: String, text2: String, color1: Color, color2: Color, @DrawableRes iconRes: Int){
+fun SourceTypeCard(text1: String,
+                   text2: String,
+                   color1: Color,
+                   color2: Color,
+                   @DrawableRes iconRes: Int,
+                   onClick: () -> Unit){
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
         shape = RoundedCornerShape(12.dp)
     ) {
         Box(modifier = Modifier
---
+++ app/src/main/java/com/apiguave/tinderclonecompose/fileprovider/ComposeFileProvider.kt
@@ -0,0 +1,30 @@
+package com.apiguave.tinderclonecompose.fileprovider
+
+import android.content.Context
+import android.net.Uri
+import androidx.core.content.FileProvider
+import com.apiguave.tinderclonecompose.R
+import java.io.File
+
+
+class ComposeFileProvider : FileProvider(
+    R.xml.filepaths
+) {
+    companion object {
+        fun getImageUri(context: Context): Uri {
+            val directory = File(context.cacheDir, "images")
+            directory.mkdirs()
+            val file = File.createTempFile(
+                "selected_image_",
+                ".jpg",
+                directory,
+            )
+            val authority = context.packageName + ".fileprovider"
+            return getUriForFile(
+                context,
+                authority,
+                file,
+            )
+        }
+    }
+}
