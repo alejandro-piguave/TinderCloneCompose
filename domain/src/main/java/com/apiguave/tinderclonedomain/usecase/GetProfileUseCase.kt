9fe5830d9c9e7a76ad90af192c5a1bd1c35a1643
Alejandro Piguave <alejandro.piguave.illescas@gmail.com>
2024-04-22 08:21:55 +0200

    fix use cases dependencies

--- domain/src/main/java/com/apiguave/tinderclonedomain/usecase/GetProfileUseCase.kt
+++ domain/src/main/java/com/apiguave/tinderclonedomain/usecase/GetProfileUseCase.kt
@@ -1,9 +1,10 @@
 package com.apiguave.tinderclonedomain.usecase
 
+import com.apiguave.tinderclonedomain.picture.PictureRepository
 import com.apiguave.tinderclonedomain.profile.ProfileRepository
 import com.apiguave.tinderclonedomain.profile.UserProfile
 
-class GetProfileUseCase(private val profileRepository: ProfileRepository) {
+class GetProfileUseCase(private val profileRepository: ProfileRepository, private val pictureRepository: PictureRepository) {
 
     suspend operator fun invoke(): Result<UserProfile> {
         return Result.runCatching { profileRepository.getProfile() }
