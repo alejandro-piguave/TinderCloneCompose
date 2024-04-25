a0a7caa7ab764750dc7a484401cf5778e644a746
Alejandro Piguave <alejandro.piguave.illescas@gmail.com>
2024-04-25 11:50:51 +0200

    fix AuthLocalDataSourceImpl error

--- data/src/main/java/com/apiguave/tinderclonedata/source/auth/AuthLocalDataSourceImpl.kt
+++ data/src/main/java/com/apiguave/tinderclonedata/source/auth/AuthLocalDataSourceImpl.kt
@@ -2,12 +2,11 @@ package com.apiguave.tinderclonedata.source.auth
 
 import com.apiguave.tinderclonedata.repository.auth.AuthLocalDataSource
 import com.apiguave.tinderclonedata.source.local.AuthProvider
-import com.google.firebase.auth.FirebaseAuth
 
 class AuthLocalDataSourceImpl(private val authProvider: AuthProvider): AuthLocalDataSource {
     override val isUserSignedIn: Boolean
         get() = authProvider.isUserSignedIn
 
     override val userId: String?
-        get() = FirebaseAuth.getInstance().currentUser?.uid
+        get() = authProvider.userId
 }
