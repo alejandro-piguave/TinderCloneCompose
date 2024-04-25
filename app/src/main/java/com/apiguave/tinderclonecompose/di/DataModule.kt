e66841424dfcf0dc6afeb51719d8a960f0b2a25d
Alejandro Piguave <alejandro.piguave.illescas@gmail.com>
2024-04-25 11:45:27 +0200

    fix dependency injection

--- app/src/main/java/com/apiguave/tinderclonecompose/di/DataModule.kt
+++ app/src/main/java/com/apiguave/tinderclonecompose/di/DataModule.kt
@@ -1,5 +1,7 @@
 package com.apiguave.tinderclonecompose.di
 
+import com.apiguave.tinderclonedata.repository.auth.AuthLocalDataSource
+import com.apiguave.tinderclonedata.repository.auth.AuthRemoteDataSource
 import com.apiguave.tinderclonedata.source.auth.AuthRemoteDataSourceImpl
 import com.apiguave.tinderclonedomain.auth.AuthRepository
 import com.apiguave.tinderclonedata.source.api.auth.AuthApi
@@ -10,9 +12,12 @@ import com.apiguave.tinderclonedata.source.api.picture.PictureApi
 import com.apiguave.tinderclonedata.source.api.user.UserApi
 import com.apiguave.tinderclonedata.source.auth.AuthLocalDataSourceImpl
 import com.apiguave.tinderclonedata.repository.auth.AuthRepositoryImpl
+import com.apiguave.tinderclonedata.repository.match.MatchRemoteDataSource
 import com.apiguave.tinderclonedata.repository.match.MatchRepositoryImpl
+import com.apiguave.tinderclonedata.repository.message.MessageRemoteDataSource
 import com.apiguave.tinderclonedata.repository.message.MessageRepositoryImpl
 import com.apiguave.tinderclonedata.repository.profile.ProfileGeneratorImpl
+import com.apiguave.tinderclonedata.repository.profile.ProfileRemoteDataSource
 import com.apiguave.tinderclonedata.repository.profile.ProfileRepositoryImpl
 import com.apiguave.tinderclonedata.source.match.MatchRemoteDataSourceImpl
 import com.apiguave.tinderclonedomain.match.MatchRepository
@@ -34,20 +39,20 @@ val dataModule = module {
     single { MessageApi(get()) }
 
     //Profile
-    single { ProfileRemoteDataSourceImpl(get(), get()) }
+    single<ProfileRemoteDataSource> { ProfileRemoteDataSourceImpl(get(), get()) }
     single<ProfileGenerator> { ProfileGeneratorImpl(get()) }
     single<ProfileRepository> { ProfileRepositoryImpl(get()) }
 
     //Messages
-    single { MessageRemoteDataSourceImpl(get(), get()) }
+    single<MessageRemoteDataSource> { MessageRemoteDataSourceImpl(get(), get()) }
     single<MessageRepository> { MessageRepositoryImpl(get()) }
 
     //Matches
-    single { MatchRemoteDataSourceImpl(get(), get(), get(), get()) }
+    single<MatchRemoteDataSource> { MatchRemoteDataSourceImpl(get(), get(), get(), get()) }
     single<MatchRepository> { MatchRepositoryImpl(get()) }
 
     //Auth
-    single { AuthLocalDataSourceImpl(get()) }
-    single { AuthRemoteDataSourceImpl(get()) }
+    single<AuthLocalDataSource> { AuthLocalDataSourceImpl(get()) }
+    single<AuthRemoteDataSource> { AuthRemoteDataSourceImpl(get()) }
     single<AuthRepository> { AuthRepositoryImpl(get(), get()) }
 }
